070472ec3b58de86d22e0ed1806ac23631de8edc
mutedsetback <bcrawford2028@kehillahstudent.org>
2026-02-16 22:54:25 -0800

    done q1 and q2

--- q1.py
+++ q1.py
@@ -1,3 +1,5 @@
+import random
+
 def spin_twister_spinner():
   """
   Returns a list with a random color, side, and appendage
@@ -7,8 +9,15 @@ def spin_twister_spinner():
   appendage: "hand" or "foot"
   """
   #YOUR CODE HERE
-  return
+  colors = ["red", "green", "yellow", "blue"]
+  sides = ["left", "right"]
+  appendages = ["hand", "foot"]
 
+  color = random.choice(colors)
+  side = random.choice(sides)
+  appendage = random.choice(appendages)
+
+  return (f"{side} {appendage} on {color}")
 # Here's the function call. This should print a random assortment of twister commands
 for _ in range(10):
   print(spin_twister_spinner())
--- q2.py
+++ q2.py
@@ -1,14 +1,21 @@
 # Make a random pet.
-
+import random
 # Choose:
 # Type of animal (at least 3 choices, string)
-animal = None#REPLACE THIS WITH YOUR CODE
+animal = ["cat", "dog", "rabbit", "horse", "turtle", "hamster", "guinea pig"]
 # Age (integer)
-age = None#REPLACE THIS WITH YOUR CODE
+age = [3, 5, 7, 1, 10, 34, 19]
 # Color (at least 3 choices, string)
-animal = None#REPLACE THIS WITH YOUR CODE
+animalcolor = ["black", "white", "brown", "carmel", "spotted", "striped"]
 # Weight (float)
-weight = None#REPLACE THIS WITH YOUR CODE
+weight = [18, 25, 30, 12, 43, 52, 132, 184]
+
+
+animal = random.choice(animal)
+age = random.choice(age)
+animalcolor = random.choice(animalcolor)
+weight = random.choice(weight)
+
 
 # Print a summary of your pet using an f-string
-print(f"Your pet is a {animal}")#REPLACE THIS WITH YOUR CODE
+print(f"Your pet is a {age} year old {animalcolor} {animal} that weighs {weight} pounds")
